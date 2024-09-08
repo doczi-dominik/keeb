9769166e09c116a537486e5737f1e4552a1e1f5d
doczi-dominik <doczi.dominik0@gmail.com>
2024-09-08 05:09:06 +0200

    ➕ Added hxbit

--- khafile.js
+++ khafile.js
@@ -1,6 +1,7 @@
 let project = new Project('Keeb');
 project.addSources('Sources');
 
+project.addLibrary('hxbit');
 project.addLibrary('safety');
 project.addParameter('--macro nullSafety("keeb", Strict)');
 
